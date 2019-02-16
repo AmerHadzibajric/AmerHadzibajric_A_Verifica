250e6e080d575fc10485dd9c855c5e14933d4d0e
Amer <amer.amer9211@gmail.com>
2019-02-16 08:41:17 +0100

    Prova tempi led

--- AmerHadzibajric_Verifica/AmerHadzibajric_Verifica.ino
+++ AmerHadzibajric_Verifica/AmerHadzibajric_Verifica.ino
@@ -3,12 +3,16 @@ int giallo = 11;
 int bianco = 7;
 int verde = 5;
 int trosso = 0;
+int tgiallo = 0;
+int tbianco = 0;
+int tverde = 0;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(rosso,OUTPUT)
-  pinMode(giallo,OUTPUT)
-  pinMode(bianco, OUTPUT)
-  pinMode(verde, OUTPUT)
+  pinMode(rosso,OUTPUT);
+  pinMode(giallo,OUTPUT);
+  pinMode(bianco, OUTPUT);
+  pinMode(verde, OUTPUT);
+  RichiestaRosso();
 
 }
 
@@ -16,6 +20,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(rosso,HIGH);
   delay(trosso);
+  digitalWrite(rosso,LOW);
+  digitalWrite(giallo,HIGH);
+  delay(tgiallo);
+ digitalWrite(rosso,LOW);
+ digitalWrite(giallo,LOW);
+ digitalWrite(bianco,HIGH);
+ delay(tbianco);
+ digitalWrite(rosso,LOW);
+ digitalWrite(giallo,LOW);
+ digitalWrite(bianco,LOW);
+ digitalWrite(verde,HIGH);
+ delay(tverde);
+
+ 
   
 
 }
@@ -23,7 +41,28 @@ void loop() {
 void RichiestaRosso()
 {
   Serial.begin(9600);
-  Serial.println("QUANTO VUOI CHE DURI IL ROSSO?")
+  Serial.println("QUANTO VUOI CHE DURI IL ROSSO?");
   while(Serial.available()==0){};
-  trosso = Serial.readString().toInt()/1000;
+  trosso = Serial.readString().toInt();
+}
+void RichiestaGiallo()
+{
+  Serial.begin(9600);
+  Serial.println("QUANTO VUOI CHE DURI IL ROSSO?");
+  while(Serial.available()==0){};
+  tgiallo = Serial.readString().toInt();
+}
+void RichiestaBianco()
+{
+  Serial.begin(9600);
+  Serial.println("QUANTO VUOI CHE DURI IL ROSSO?");
+  while(Serial.available()==0){};
+  tbianco = Serial.readString().toInt();
+}
+void RichiestaVerde()
+{
+  Serial.begin(9600);
+  Serial.println("QUANTO VUOI CHE DURI IL ROSSO?");
+  while(Serial.available()==0){};
+  tverde = Serial.readString().toInt();
 }
